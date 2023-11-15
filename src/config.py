125a51100119c15691cad2e43ef54e25410509f1
Roman Frolov <frolov.ra@obt-vlg.ru>
2023-11-15 19:32:21 +0300

    main - Из конфига убраны неиспользуемые переменные

--- src/config.py
+++ src/config.py
@@ -30,9 +30,3 @@ ozon_domain = 'https://api-seller.ozon.ru/'
 ozon_product_import_url = ozon_domain + 'v2/product/import'
 ozon_update_product_prices_url = ozon_domain + 'v1/product/import/prices'
 ozon_update_product_stocks_url = ozon_domain + 'v1/product/import/stocks'
-
-ozon_client_id = os.getenv('OZON_CLIENT_ID')
-ozon_api_key = os.getenv('OZON_API_KEY')
-
-if not (ozon_client_id and ozon_api_key):
-    raise ValueError('Не заданы Client-Id и Api-Key для доступа к api озона.')
